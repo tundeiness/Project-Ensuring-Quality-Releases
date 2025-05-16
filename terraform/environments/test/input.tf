07f8721e10d2f2d2e9402dc04c924016071c5e10
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-05-15 22:44:25 -0400

    updated new files

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,18 +1,40 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  type = string
+}
+variable "client_id" {
+  type = string
+}
+variable "client_secret" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
+variable "location" {
+  type = string
+}
+variable "resource_group" {
+  type = string
+}
+variable "application_type" {
+  type = string
+}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {
+  type = string
+}
+variable "address_prefix_test" {
+  type = string
+}
+variable "address_space" {
+  type = list(string)
+}
 
 # VM Inputs
-variable "vm_name" {}
+variable "vm_name" {
+  type = string
+}
