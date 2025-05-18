fe45d8288b9a060038378060e51396b2b2bee803
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-05-18 18:06:18 -0400

    edit input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -27,12 +27,8 @@ variable "application_type" {
 variable "virtual_network_name" {
   type = string
 }
-variable "address_prefix_test" {
-  type = string
-}
-variable "address_space" {
-  type = list(string)
-}
+variable "address_prefix_test" {}
+variable "address_space" {}
 
 # VM Inputs
 variable "vm_name" {
