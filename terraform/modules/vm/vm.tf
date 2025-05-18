1820eccf589dec4dcebed2deec577bed48a322af
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-05-18 15:49:01 -0400

    revert

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "vm_nic" {
-  name                = "myAzurenic"
+  name                = "${var.application_type}-${var.resource_type}-nic"
   location            = var.location
   resource_group_name = var.resource_group
 
