19688c6f537e7619f9fd943f7607ef28074ccb57
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-05-24 16:36:30 -0400

    output value for public Ip

--- terraform/environments/test/outputs.tf
+++ terraform/environments/test/outputs.tf
@@ -5,3 +5,9 @@ output "vm_name" {
 output "vm_private_ip" {
   value = module.vm.private_ip_address
 }
+
+output "vm_public_ip" {
+  description = "The public IP address of the VM"
+  # value       = azurerm_public_ip.public_ip.ip_address
+  value = module.vm.public_ip_address_id
+}
