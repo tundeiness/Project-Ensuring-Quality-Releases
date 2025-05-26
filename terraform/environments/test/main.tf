5c30f7cc83a76a547a96c5587d6038791a2b1e01
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-05-26 17:07:22 -0400

    add location, add state

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
+    storage_account_name = "tfstatecc9ded810294"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
   }
